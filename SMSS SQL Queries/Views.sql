4cb3bda0fcf4a3b7e05b9ea662a06bfb764bf261
Abbas-A-Hadi <abbas133999@gmail.com>
2025-05-16 11:57:12 +0300

    Update Views.sql

--- SMSS SQL Queries/Views.sql
+++ SMSS SQL Queries/Views.sql
@@ -1,4 +1,3 @@
-
 USE DVLD;
 GO
 
@@ -6,39 +5,37 @@ SELECT * FROM People;
 GO
 
 
---CREATE VIEW People_View AS 
---SELECT p.PersonID, p.NationalNo, p.FirstName, p.SecondName, p.ThirdName, p.LastName, 
---	   p.DateOfBirth, p.Gender, p.Phone, p.Email, Nationality = c.CountryName, p.ImagePath
---FROM People p 
---INNER JOIN Countries c ON p.NationalityCountryID = c.CountryID;
---GO
+CREATE VIEW People_View AS
+SELECT [Person ID] = p.PersonID, [National No] = p.NationalNo, [First Name] = p.FirstName, [Second Name] = p.SecondName, 
+	   [Third Name] = p.ThirdName, [Last Name] = p.LastName, [Date Of Birth] = p.DateOfBirth, [Nationality] = c.CountryName, 
+	   Gender = CASE p.Gender WHEN 0 THEN 'Male' ELSE 'Female' END, p.Phone, p.Email
+FROM People p INNER JOIN Countries c ON p.NationalityCountryID = c.CountryID;
+GO
 
 SELECT * FROM People_View;
 GO
 
 
---CREATE VIEW UsersWithFullInfo_View AS
---SELECT u.UserID, u.UserName, u.Password, p.NationalNo, p.FirstName, p.SecondName, p.ThirdName, p.LastName, 
---	   p.DateOfBirth, p.Gender, p.Address, p.Phone, p.Email, Nationality = c.CountryName, p.ImagePath 
---FROM Users u 
---INNER JOIN People p ON u.PersonID = p.PersonID
---INNER JOIN Countries c ON p.NationalityCountryID = c.CountryID;
---GO
+CREATE VIEW UsersWithFullInfo_View AS
+SELECT u.UserID, u.UserName, u.Password, p.NationalNo, p.FirstName, p.SecondName, p.ThirdName, p.LastName, 
+	   p.DateOfBirth, p.Gender, p.Address, p.Phone, p.Email, Nationality = c.CountryName, p.ImagePath 
+FROM Users u 
+INNER JOIN People p ON u.PersonID = p.PersonID
+INNER JOIN Countries c ON p.NationalityCountryID = c.CountryID;
+GO
 
 SELECT * FROM UsersWithFullInfo_View;
 GO
 
 
---CREATE VIEW Users_View AS
---SELECT u.UserID, u.PersonID, FullName = p.FirstName + ' ' + p.SecondName + ' ' + (
---		CASE 
---		WHEN TRIM(p.ThirdName) != '' OR p.ThirdName IS NOT NULL THEN p.ThirdName + ' '
---		ELSE '' END ) + p.LastName, u.UserName, u.IsActive
---FROM Users u
---INNER JOIN People p ON u.PersonID = p.PersonID;
---GO
+CREATE VIEW Users_View AS
+SELECT u.UserID, u.PersonID, FullName = p.FirstName + ' ' + p.SecondName + ' ' + (
+		CASE 
+		WHEN TRIM(p.ThirdName) != '' OR p.ThirdName IS NOT NULL THEN p.ThirdName + ' '
+		ELSE '' END ) + p.LastName, u.UserName, u.IsActive
+FROM Users u
+INNER JOIN People p ON u.PersonID = p.PersonID;
+GO
 
 SELECT * FROM Users_View;
 GO
-
-
